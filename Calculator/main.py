e06eedd437341bd8a4df514f82732e3259e1eaa1
Daryna <89580249+darynanik@users.noreply.github.com>
2022-01-16 23:44:30 +0200

    removing unnecessary code and beautifying

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,58 +1,56 @@
 from simpleeval import simple_eval
-count = 2
-inputs = []
-variable_count = input('Enter element count:')
 
-operations = ['+', '-', '*', '/']
+count = 2
+variable_count = input('Enter element count:')
+operations = ['+', '-', '*', '/', '//', '**']
+
 
 def calculate(expr):
+    result = expr
     try:
         result = simple_eval(expr)
     except ZeroDivisionError as err:
         print(f'You cannot divide by zero. Error type {err}')
-        exit(0)
-    return result   
-def check_operation(operation):
-    if operation not in operations:
+        exit(1)
+    return result
+
+
+def check_operation(operator):
+    if operator not in operations:
         print('Operation was not found.')
-        exit(0)          
-def check_user_input(input):
-    val = input
+        exit(1)
+
+
+def parse_user_input(user_input):
+    val = user_input
     try:
-        # Convert it into integer
-        val = int(input)
+        val = float(user_input)
     except ValueError:
-        try:
-            # Convert it into float
-            val = float(input)
-        except ValueError:
-            print(f"No.. input is not a number. It's a string, input value was {val}.")
-            exit(0)
+        print(f"No.. input is not a number. It's a string, input value was {val}.")
+        exit(1)
     return val
 
-expression = ''
-variable_count = check_user_input(variable_count)
-var_1 =   input(f'Enter var1:')
-check_user_input(var_1)
+
+variable_count = parse_user_input(variable_count)
+var_1 = input(f'Enter var1:')
+parse_user_input(var_1)
 
 operation = input(f'Enter operation1:')
 check_operation(operation)
 
-var_2 =  input(f'Enter var2:')
-check_user_input(var_2)
-inputs.extend([var_1, operation, var_2])
-expression = expression.join(inputs)
+var_2 = input(f'Enter var2:')
+parse_user_input(var_2)
+expression = f'{var_1}{operation}{var_2}'
 
 if variable_count > 2:
     while count < variable_count:
         operation = input(f'Enter operation{count}:')
         check_operation(operation)
-         
-        var = input(f'Enter var{count+1}:')
-        check_user_input(var)
-        
-        inputs.extend([operation, var])
-        expression = ''.join(inputs)
+
+        var = input(f'Enter var{count + 1}:')
+        parse_user_input(var)
+
+        expression = f'{expression}{operation}{var}'
         count += 1
- 
+
 print('Result:', calculate(expression))
